ea63bd4ce2896ddb0441886d0e0a85edf3e57491
marco9333 <marco.magliona@gmail.com>
2014-02-16 10:36:28 +0100

    n8000: add default apn list to build

--- omni_n8000.mk
+++ omni_n8000.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
