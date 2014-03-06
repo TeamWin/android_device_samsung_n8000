d5c42007aa20ee4c19b9d1a070cbea81dd86d4a4
Markus Guidry <markus@gu1dry.com>
2014-03-06 14:13:08 -0500

    n8000: Align ro.product with stock
    Additionally update fingerprint to newer fingerpint.
    
      PS7: ro.product.device is actually set with TARGET_DEVICE & not
           PRODUCT_DEVICE; this has been corrected.
      PS8: PRODUCT_RELEASE_NAME isn't used in omnirom.
      PS9: Fix rebase issues
    Change-Id: I759778d95d7b93b496b5889ea9fdad9ac60900f0

--- omni_n8000.mk
+++ omni_n8000.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2013 The OmniROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,26 +12,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# This is where we'd set a backup provider if we had one
 
 # Inherit from our omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/n8000/device.mk)
 
 # Discard inherited values and use our own instead.
+PRODUCT_MODEL := GT-N8000
+PRODUCT_BRAND := samsung
 PRODUCT_NAME := omni_n8000
 PRODUCT_DEVICE := n8000
-PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-N8000
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-N8000 TARGET_DEVICE=GT-N8000 BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.1.2/JZO54K/N8000XXCML2:user/release-keys" PRIVATE_BUILD_DESC="p4noterfxx-user 4.1.2 JZO54K N8000XXCML2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=p4noterfxx \
+    TARGET_DEVICE=p4noterf \
+    PRIVATE_BUILD_DESC="p4noterfxx-user 4.1.2 JZO54K N8000XXCML2 release-keys" \
+    BUILD_FINGERPRINT="samsung/p4noterfxx/p4noterf:4.1.2/JZO54K/N8000XXCML2:user/release-keys"
