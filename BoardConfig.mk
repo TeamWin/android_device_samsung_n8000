7abc89665899e76673f13933db5ce27631bb6c14
lirokoa <lirokoa@hotmail.com>
2014-12-16 17:53:36 +0100

    Disable selinux for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,17 +30,17 @@ TARGET_OTA_ASSERT_DEVICE := c0,p4noterf,n8000,GT-N8000
 
 
 # Selinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/n8000/selinux
+# BOARD_SEPOLICY_DIRS += \
+#     device/samsung/n8000/selinux
 
-BOARD_SEPOLICY_UNION += \
-    device.te \
-    domain.te \
-    file.te \
-    file_contexts \
-    init.te \
-    mediaserver.te \
-    rild.te \
-    system.te \
-    ueventd.te \
-    wpa_supplicant.te
+# BOARD_SEPOLICY_UNION += \
+#     device.te \
+#     domain.te \
+#     file.te \
+#     file_contexts \
+#     init.te \
+#     mediaserver.te \
+#     rild.te \
+#     system.te \
+#     ueventd.te \
+#     wpa_supplicant.te
