4a35109c483a2a2560d045665b6a810bd9fa509e
lirokoa <lirokoa@hotmail.com>
2014-12-16 17:53:49 +0100

    update mobile data interfaces

--- n8000.mk
+++ n8000.mk
@@ -41,7 +41,7 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril_class=SamsungExynos4RIL \
-    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
+    mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1 \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10
 
