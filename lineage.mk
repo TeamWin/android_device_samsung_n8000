7b3ab117f6c4fa670ed5ea23bf0857293cb1f6fa
html6405 <peter.schelchshorn@zell-net.at>
2020-07-03 09:48:49 +0200

    Added new screen resolution for the boot animation.
    Change-Id: I283a06c712a64d720e04a94092816c37ac040cd7

--- lineage.mk
+++ lineage.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 600
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8000
 PRODUCT_NAME := lineage_n8000
